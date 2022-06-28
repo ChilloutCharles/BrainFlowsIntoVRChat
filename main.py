dfd8303c6278d9a1898c17a3e1ac7baec15d168e
chillout_charles <blargophobia@gmail.com>
2022-06-28 15:42:02 -0500

    disable debug logging by default

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
     DataFilter.enable_data_logger()
 
     ### Uncomment this to see debug messages ###
-    BoardShim.set_log_level(LogLevels.LEVEL_DEBUG.value)
+    # BoardShim.set_log_level(LogLevels.LEVEL_DEBUG.value)
 
     ### Paramater Setting ###
     parser = argparse.ArgumentParser()
