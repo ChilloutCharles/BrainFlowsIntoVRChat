8b0c1698a3433c0f20f4476656be8da66af8fb99
Chillout Charles <blargophobia@gmail.com>
2024-01-21 01:45:58 -0600

    hr and resp rewrite

--- logic/heartrate.py
+++ logic/heartrate.py
@@ -1,7 +1,10 @@
 from logic.base_logic import Base_Logic
 
 from brainflow.board_shim import BoardShim, BrainFlowPresets
-from brainflow.data_filter import DataFilter, DetrendOperations, AggOperations
+from brainflow.data_filter import DataFilter, AggOperations, NoiseTypes, FilterTypes, DetrendOperations, WindowOperations
+
+import copy
+import numpy as np
 
 class HeartRate(Base_Logic):
     def __init__(self, board, fft_size=1024):
@@ -18,25 +21,73 @@ class HeartRate(Base_Logic):
         self.max_sample_size = self.ppg_sampling_rate * self.window_seconds
         self.fft_size = fft_size
 
+    def estimate_respiration(self, resp_signal):
+        # Possible min and max respiration in hz
+        lowcut = 0.1
+        highcut = 0.5
+
+        # Detrend the signal to remove linear trends
+        DataFilter.detrend(resp_signal, DetrendOperations.LINEAR.value)
+
+        # filter down to possible respiration rates
+        DataFilter.perform_bandpass(resp_signal, self.ppg_sampling_rate, lowcut, highcut, 3, FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
+
+        # Perform FFT
+        fft_data = DataFilter.perform_fft(resp_signal, WindowOperations.NO_WINDOW.value)
+        fft_freq = np.linspace(0, self.ppg_sampling_rate / 2, len(fft_data) // 2)
+
+        # Find the peak frequency in the respiratory range
+        idx = np.where((fft_freq >= lowcut) & (fft_freq <= highcut))
+        peak_freq = fft_freq[idx][np.argmax(np.abs(fft_data[idx]))]
+
+        # Return breathing rate in BPM
+        return peak_freq * 60
+
+    def estimate_heart_rate(self, hr_ir, hr_red):
+        # Possible min and max respiration in hz
+        lowcut = 0.1
+        highcut = 4.25
+
+        # Detrend the signal to remove linear trends
+        DataFilter.detrend(hr_ir, DetrendOperations.LINEAR.value)
+        DataFilter.detrend(hr_red, DetrendOperations.LINEAR.value)
+
+        # filter down to possible heart rates
+        DataFilter.perform_bandpass(hr_ir, self.ppg_sampling_rate, lowcut, highcut, 2, FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
+        DataFilter.perform_bandpass(hr_red, self.ppg_sampling_rate, lowcut, highcut, 2, FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
+
+        ### Brainflow Heart Example ###
+        ### https://github.com/brainflow-dev/brainflow/blob/master/python_package/examples/tests/muse_ppg.py ###
+        heart_bpm = DataFilter.get_heart_rate(hr_ir, hr_red, self.ppg_sampling_rate, self.fft_size)
+        heart_bps = heart_bpm / 60
+
+        return heart_bps, int(heart_bpm + 0.5)
+
     def get_data_dict(self):
         # get current data from board
         ppg_data = self.board.get_current_board_data(
             self.max_sample_size, BrainFlowPresets.ANCILLARY_PRESET)
         
-        # get ir and red channels
-        ppg_ir = ppg_data[self.ppg_channels[1]]
-        ppg_red = ppg_data[self.ppg_channels[0]]
+        # get ambient, ir, red channels, and clean the channels with ambient
+        ppg_ambient = ppg_data[self.ppg_channels[2]]
+        ppg_ir = ppg_data[self.ppg_channels[1]] - ppg_ambient
+        ppg_red = ppg_data[self.ppg_channels[0]] - ppg_ambient
 
-        # calculate heartrate and oxygen level from channels
+        # calculate oxygen level
         oxygen_level = DataFilter.get_oxygen_level(ppg_ir, ppg_red, self.ppg_sampling_rate) * 0.01
-        ### Brainflow Heart Example ###
-        ### https://github.com/brainflow-dev/brainflow/blob/master/python_package/examples/tests/muse_ppg.py ###
-        heart_bpm = DataFilter.get_heart_rate(ppg_ir, ppg_red, self.ppg_sampling_rate, self.fft_size)
-        heart_bps = heart_bpm / 60
 
+        # calculate heartrate
+        heart_bps, heart_bpm = self.estimate_heart_rate(copy.deepcopy(ppg_ir), copy.deepcopy(ppg_red))
+
+        # calculate respiration
+        resp_ir = self.estimate_respiration(copy.deepcopy(ppg_ir))
+        resp_red = self.estimate_respiration(copy.deepcopy(ppg_red))
+        resp_avg = int(np.mean((resp_ir, resp_red)) + 0.5)
+        
         # format as dictionary
         return {
             "osc_oxygen_percent" : oxygen_level,
             "osc_heart_bps" : heart_bps,
-            "osc_heart_bpm" : int(heart_bpm + 0.5)
+            "osc_heart_bpm" : heart_bpm,
+            "osc_respiration_bpm" : resp_avg
         }
