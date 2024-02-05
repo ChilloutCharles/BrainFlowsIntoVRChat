bbe4673d922faf7c87f094e3aeba2b81217002a8
Chillout Charles <blargophobia@gmail.com>
2024-02-04 21:47:11 -0600

    draft model creation

---
+++ model/intent/test.py
@@ -0,0 +1,103 @@
+import argparse
+import time
+import pickle
+
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
+from brainflow.data_filter import DataFilter, DetrendOperations, NoiseTypes, WaveletTypes
+
+
+from sklearn import svm
+from scipy.stats import kurtosis
+import numpy as np
+
+import pickle
+
+window_seconds = 1
+
+def main():
+    with open("model.ml", "rb") as f:
+        clf = pickle.load(f)
+    print(clf.classes_)
+
+    parser = argparse.ArgumentParser()
+    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=True)
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    parser.add_argument('--master-board', type=int, help='master board id for streaming and playback boards',
+                        required=False, default=BoardIds.NO_BOARD)
+    args = parser.parse_args()
+
+    params = BrainFlowInputParams()
+    params.ip_port = args.ip_port
+    params.serial_port = args.serial_port
+    params.mac_address = args.mac_address
+    params.other_info = args.other_info
+    params.serial_number = args.serial_number
+    params.ip_address = args.ip_address
+    params.ip_protocol = args.ip_protocol
+    params.timeout = args.timeout
+    params.file = args.file
+    params.master_board = args.master_board
+
+    board = BoardShim(args.board_id, params)
+
+    sampling_rate = BoardShim.get_sampling_rate(args.board_id)
+    eeg_channels = BoardShim.get_eeg_channels(args.board_id)
+    sampling_size = sampling_rate * window_seconds
+
+    board.prepare_session()
+    board.start_stream()
+
+    # 1. wait 5 seconds before starting
+    print("Get ready in {} seconds".format(window_seconds))
+    time.sleep(2)
+
+    current_value = 0
+
+    while True:
+        data = board.get_current_board_data(sampling_size)
+
+        # detrend and denoise
+        for eeg_chan in eeg_channels:
+            DataFilter.remove_environmental_noise(data[eeg_chan], sampling_rate, NoiseTypes.FIFTY_AND_SIXTY.value)
+            DataFilter.detrend(data[eeg_chan], DetrendOperations.LINEAR)
+        
+        # Independent Component Analysis and selection through kurtosis threshold
+        _, _, _, source_signals = DataFilter.perform_ica(data, 3, eeg_channels)
+        kurtosis_threshold = 3
+        kurtoses = [np.abs(kurtosis(component)) for component in source_signals]
+        kurtoses = [k if k < kurtosis_threshold else 0 for k in kurtoses]
+        comp_idx = np.argmax(kurtoses)
+        data[eeg_channels] = source_signals[comp_idx]
+
+        # Wavelet Transform on signal
+        intent_eeg = data[eeg_channels[0]]
+        intent_wavelet_coeffs, intent_lengths = DataFilter.perform_wavelet_transform(intent_eeg, WaveletTypes.DB4, 5)
+
+        # only look at detailed parts which will contain the higher frequencies
+        intent_wavelet_coeffs = intent_wavelet_coeffs[intent_lengths[0] : ]
+
+        pred_string = clf.predict([intent_wavelet_coeffs])
+        target_value = 1.0 if pred_string[0] == 'button' else 0.0
+        ema_value = 0.03
+        current_value = current_value * (1 - ema_value) + target_value * ema_value
+
+        string = "^" if current_value > 0.5 else "*"
+        visual = string * int(50 * current_value)
+        print("{}\t{}".format(pred_string, visual))
+
+        time.sleep(1/60)
+
+if __name__ == "__main__":
+    main()
--- model/intent/train.py
+++ model/intent/train.py
@@ -8,10 +8,14 @@ from brainflow.board_shim import BoardShim
 from brainflow.data_filter import DataFilter, DetrendOperations, NoiseTypes, WaveletTypes
 
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV 
+from sklearn.metrics import classification_report, confusion_matrix 
+from scipy.stats import kurtosis
 
 import pickle
 import random
 import numpy as np
+
 
 with open("recorded_eeg.pkl", "rb") as f:
     recorded_data = pickle.load(f)
@@ -25,15 +29,22 @@ sampling_rate = BoardShim.get_sampling_rate(board_id)
 eeg_channels = BoardShim.get_eeg_channels(board_id)
 
 for i, data in enumerate(intent_data + baseline_data):
+    # detrend and denoise
     for eeg_chan in eeg_channels:
         assert len(data[eeg_chan]) == sampling_rate * record_seconds
         DataFilter.remove_environmental_noise(data[eeg_chan], sampling_rate, NoiseTypes.FIFTY_AND_SIXTY.value)
         DataFilter.detrend(data[eeg_chan], DetrendOperations.LINEAR)
-
-
-window_seconds = 2
-train_size = 500
-test_size = 50
+    
+    # Independent Component Analysis and selection through kurtosis threshold
+    _, _, _, source_signals = DataFilter.perform_ica(data, 3, eeg_channels)
+    kurtosis_threshold = 3
+    kurtoses = [np.abs(kurtosis(component)) for component in source_signals]
+    kurtoses = [k if k < kurtosis_threshold else 0 for k in kurtoses]
+    comp_idx = np.argmax(kurtoses)
+    data[eeg_channels] = source_signals[comp_idx]
+    
+window_seconds = 1
+data_size_multiplier = 0.5
 
 slice_indexes = list(range(len(intent_data)))
 random.shuffle(slice_indexes)
@@ -47,12 +58,15 @@ for intent_index in intent_indexes:
     for slice_index in train_slice_indexes:
         pair = (slice_index, intent_index)
         train_indexes.append(pair)
+
+train_size = int(len(train_indexes) * data_size_multiplier)
 train_indexes = random.sample(train_indexes, train_size)
 
 test_indexes = []
 for intent_index in intent_indexes:
     pair = (test_slice_index, intent_index)
     test_indexes.append(pair)
+test_size = int(len(test_indexes) * data_size_multiplier)
 test_indexes = random.sample(test_indexes, test_size)
 
 def create_data(indexes):
@@ -65,17 +79,20 @@ def create_data(indexes):
 
         intent_wavelets = []
         baseline_wavelets = []
-        for eeg_chan in eeg_channels:
-            intent_eeg = intent_slice[eeg_chan][i:j]
-            intent_wavelet_coeffs, intent_lengths = DataFilter.perform_wavelet_transform(intent_eeg, WaveletTypes.DB3, 6)
-            # intent_wavelet_coeffs = intent_wavelet_coeffs[intent_lengths[0]:]
+        # for eeg_chan in eeg_channels:
+        eeg_chan = eeg_channels[0]
+        intent_eeg = intent_slice[eeg_chan][i:j]
+        intent_wavelet_coeffs, intent_lengths = DataFilter.perform_wavelet_transform(intent_eeg, WaveletTypes.DB4, 5)
 
-            baseline_eeg = baseline_slice[eeg_chan][i:j]
-            baseline_wavelet_coeffs, baseline_lengths = DataFilter.perform_wavelet_transform(baseline_eeg, WaveletTypes.DB3, 6)
-            # baseline_wavelet_coeffs = baseline_wavelet_coeffs[baseline_lengths[0]:]
+        baseline_eeg = baseline_slice[eeg_chan][i:j]
+        baseline_wavelet_coeffs, baseline_lengths = DataFilter.perform_wavelet_transform(baseline_eeg, WaveletTypes.DB4, 5)
 
-            intent_wavelets.append(intent_wavelet_coeffs)
-            baseline_wavelets.append(baseline_wavelet_coeffs)
+        # only look at detailed parts which will contain the higher frequencies
+        intent_wavelet_coeffs = intent_wavelet_coeffs[intent_lengths[0] : ]
+        baseline_wavelet_coeffs = baseline_wavelet_coeffs[baseline_lengths[0] : ]
+
+        intent_wavelets.append(intent_wavelet_coeffs)
+        baseline_wavelets.append(baseline_wavelet_coeffs)
 
         intent_wavelets = np.array(intent_wavelets).flatten()
         baseline_wavelets = np.array(baseline_wavelets).flatten()
@@ -90,12 +107,23 @@ def create_data(indexes):
 
 X, y = create_data(train_indexes)
 
-clf = svm.SVC()
-clf.fit(X,y)
+param_grid = {'C': [0.1, 1, 10, 100],   
+              'gamma': [1, 0.1, 0.01, 'auto', 'scale']}
+              # 'probability': [True]}  
+grid = GridSearchCV(svm.SVC(), param_grid, refit = True, verbose = 3, n_jobs=5) 
+grid.fit(X, y) 
+print(grid.best_params_) 
+print(grid.best_estimator_) 
+clf = grid.best_estimator_
+
+
+# clf = svm.SVC(C=1, gamma='auto', probability=True)
+# clf = svm.SVC(C=10, probability=True)
+# clf.fit(X, y)
 
 test_X, test_y = create_data(test_indexes)
-test_score = clf.score(test_X, test_y)
-print(test_score)
+preds = clf.predict(test_X)
+print(classification_report(test_y, preds))
 
-
-
+with open('model.ml', 'wb') as f:
+    pickle.dump(clf, f)
