20e3a9ad2bab08f74a0c610df416238c976cd66a
Chillout Charles <blargophobia@gmail.com>
2024-05-29 20:49:49 -0500

    fixes and improvements
    reduced overlap, use old train test split, redid encoder decoder, changed classifier loss to bce

--- model/intent/model.py
+++ model/intent/model.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import keras
 
 from keras.models import Sequential
-from keras.layers import Dense, MaxPooling1D, Activation, Flatten, Layer, Multiply, BatchNormalization, Dropout
-from keras.layers import SeparableConv1D, Conv1D, UpSampling1D, Conv1DTranspose, UpSampling1D, AveragePooling1D, GlobalAveragePooling1D
+from keras.layers import Dense, MaxPooling1D, Activation, Layer, Multiply, BatchNormalization, Dropout
+from keras.layers import SeparableConv1D, Conv1D, UpSampling1D, UpSampling1D, GlobalAveragePooling1D
 
 ## Spatial Attention (Thanks Summer!)
 @keras.saving.register_keras_serializable()
@@ -23,49 +23,63 @@ class SpatialAttention(Layer):
 ## Encoder and Decoder Trained on the physionet motor imagery dataset
 ## https://www.physionet.org/content/eegmmidb/1.0.0/
 ## Thanks again to Summer, Programmerboi, Hosomi
-k1 = 5
-k2 = 3
-act = 'leaky_relu'
+k1 = 3
+k2 = 1
+act = 'silu'
 
-encoder = Sequential([ # Input Shape: (160, 64) => 10,240
+encoder = Sequential([ 
+    # Input Shape: (160, 64) => 10,240
+
+    SeparableConv1D(64, 1, padding='same'),
+    BatchNormalization(), Activation('sigmoid'),
+    
     SeparableConv1D(32, k1, padding='same'),
-    SeparableConv1D(32, k2, padding='same'), 
-    Activation(act),
+    BatchNormalization(), Activation(act),
+    SeparableConv1D(32, k2, padding='same'),
+    BatchNormalization(), Activation(act),
+
     MaxPooling1D(2), # Length: 160 / 2 = 80
 
-    SeparableConv1D(64, k1, padding='same'),
-    SeparableConv1D(64, k2, padding='same'), 
-    Activation(act),
+    SeparableConv1D(32, k1, padding='same'),
+    BatchNormalization(), Activation(act),
+    SeparableConv1D(32, k2, padding='same'),
+    BatchNormalization(), Activation(act),
 
-    SpatialAttention(7) # Output Shape: (80, 64) => 5,120
+    SpatialAttention(7)
+
+    # Output Shape: (80, 32) => 2,560
 ])
 
 decoder = Sequential([
-    Conv1DTranspose(64, k2, padding='same'), 
-    Conv1DTranspose(64, k1, padding='same'), 
-    Activation(act),
+    SeparableConv1D(32, k2, padding='same'),
+    BatchNormalization(), Activation(act),
+    SeparableConv1D(32, k1, padding='same'),
+    BatchNormalization(), Activation(act),
 
     UpSampling1D(2),
-    Conv1DTranspose(32, k2, padding='same'), 
-    Conv1DTranspose(32, k1, padding='same'), 
-    Activation(act),
 
-    # Reconstruct 64 channels from original input
-    Conv1D(64, 1, activation='sigmoid', padding='valid')
+    SeparableConv1D(32, k2, padding='same'),
+    BatchNormalization(), Activation(act),
+    SeparableConv1D(32, k1, padding='same'),
+    BatchNormalization(), Activation(act),
+
+    SeparableConv1D(64, 1, padding='same'),
+    BatchNormalization(), Activation('sigmoid'),
 ])
 
 ## First Layer to convert any channels to 64 ranged [0, 1]
 def create_first_layer(channels):
     return Sequential([
         Conv1D(64, channels, padding='same'),
+        BatchNormalization(),
         Activation('sigmoid'),
     ])
 
 ## Last Layer to map latent space to custom classes
 def create_last_layer(classes):
     return Sequential([
-        Dropout(0.1),
         BatchNormalization(),
         GlobalAveragePooling1D(),
+        Dropout(0.1),
         Dense(classes, activation='softmax')
     ])
--- model/intent/train.py
+++ model/intent/train.py
@@ -11,6 +11,7 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from keras.utils import to_categorical
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 from model import create_first_layer, create_last_layer
 from pipeline import preprocess_data, extract_features
@@ -39,17 +40,14 @@ def main():
 
     action_dict = recorded_data['action_dict']
     window_size = int(1.0 * sampling_rate)
-    overlap = int(window_size * 0.93)
+    overlap = int(window_size * 0.9)
 
-    # train and test windows seperated out to prevent overlap
     def windows_from_datas(datas):
         eegs = [data[eeg_channels] for data in datas]
         windows_per_session = [segment_data(eeg, window_size, overlap) for eeg in eegs]
-        random.shuffle(windows_per_session)
-        windows, windows_test = windows_per_session[1:], [windows_per_session[0]]
-        return np.concatenate(windows), np.concatenate(windows_test)
+        windows = np.concatenate(windows_per_session)
+        return windows
     
-    # dictionary of tuples containing train and test windows
     action_windows = {k:windows_from_datas(datas) for k, datas in action_dict.items()}
 
     ## extract the features from the windows
@@ -61,20 +59,14 @@ def main():
             feature_windows.append(features)
         return feature_windows
     
-    processed_windows = {k:(process_windows(train),process_windows(test)) for k, (train, test) in action_windows.items()}
+    processed_windows = {k:process_windows(windows) for k, windows in action_windows.items()}
     
     ## create train and test sets and labels
-    i_train = np.concatenate([[k] * len(windows_train) for k, (windows_train, _) in processed_windows.items()])
-    shuffle_indexes = list(range(len(i_train)))
-    random.shuffle(shuffle_indexes)
-    X_train = np.concatenate([windows_train for windows_train, _ in processed_windows.values()])[shuffle_indexes]
-    y_train = to_categorical(i_train, num_classes=len(processed_windows))[shuffle_indexes]
+    indices = np.concatenate([[k] * len(v) for k, v in processed_windows.items()])
+    X = np.concatenate(list(processed_windows.values()))
+    y = to_categorical(indices, num_classes=len(processed_windows))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, shuffle=True)
 
-    i_test = np.concatenate([[k] * len(windows_test) for k, (_, windows_test) in processed_windows.items()])
-    X_test = np.concatenate([windows_test for windows_test, _ in processed_windows.values()])
-    y_test = to_categorical(i_test, num_classes=len(processed_windows))
-
-    print(X_train.shape)
 
     ## Compile the model
     classes = len(processed_windows)
@@ -89,7 +81,7 @@ def main():
         pretrained_encoder,
         create_last_layer(classes)
     ])
-    model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy')
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy')
 
     ## Set up EarlyStopping
     early_stopping = EarlyStopping(monitor='val_loss', patience=16, restore_best_weights=True, verbose=0)
