b689955f1565df6ef1b6c0c3d25719d9a1cf51dc
Chillout Charles <blargophobia@gmail.com>
2024-05-31 13:29:20 -0500

    classifier dropout

--- model/intent/model.py
+++ model/intent/model.py
@@ -59,5 +59,6 @@ def create_last_layer(classes):
     return Sequential([
         SpatialAttention(),
         GlobalAveragePooling1D(),
+        Dropout(0.2),
         Dense(classes, activation='softmax')
     ])
