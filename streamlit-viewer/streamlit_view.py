1214b321758c0969a977f4a41672e42343c4f751
_Vulpes <vulpesk1tzune@gmail.com>
2025-02-23 17:04:21 +0100

    more performance tests: myplot to slow for usage

--- streamlit-viewer/streamlit_view.py
+++ streamlit-viewer/streamlit_view.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_autorefresh import st_autorefresh
 import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -12,6 +11,10 @@ MILLISECONDS_REFRESH = 500
 TIMESTEPS_WINDOW = 256
 
 start_time = time.time()
+
+def write_elapsed_time_till_start(timedesc = ""):
+    timeElapsed = time.time() - start_time
+    st.write( timedesc + ": Time elapsed in [s]: ", timeElapsed)
 
 def run_osc_server(ip, port):
     osc_server.run_server(ip, port)  
@@ -38,14 +41,19 @@ if "Button Pressed last" not in st.session_state:
 
 
 if st.button("NeuroFB") or st.session_state["Button Pressed last"] == "NeuroFB":
+
     slice = osc_server.get_neurofb_dataframes().get_latest_frames(TIMESTEPS_WINDOW)
+    write_elapsed_time_till_start("Get NeuroFB slice")
 
     groups = [['FocusLeft', 'FocusRight', 'FocusAvg'], ['RelaxLeft', 'RelaxRight', 'RelaxAvg']]
 
     graphs, deltaTime = get_graphs_and_deltaTime_from_slice(slice)
     graphgroups = split_by_identifierGroups(graphs, groups, exclude="Pos")
 
+    write_elapsed_time_till_start("Prep NeuroFB Data")
+
     plot_all_groups_dark(graphgroups, deltaTime)
+    write_elapsed_time_till_start("Plot NeuroFB Data")
     st.session_state["Button Pressed last"] = "NeuroFB"
 
 
@@ -53,6 +61,8 @@ if st.button("PowerBands") or st.session_state["Button Pressed last"] == "PowerB
     sliceLeft = osc_server.get_pwrbands_dataframes_left().get_latest_frames(TIMESTEPS_WINDOW)
     sliceRight = osc_server.get_pwrbands_dataframes_right().get_latest_frames(TIMESTEPS_WINDOW)
     sliceAvg = osc_server.get_pwrbands_dataframes_avg().get_latest_frames(TIMESTEPS_WINDOW)
+
+    write_elapsed_time_till_start("Get PowerBands slices")
 
     groups = [['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']]
     
@@ -62,14 +72,18 @@ if st.button("PowerBands") or st.session_state["Button Pressed last"] == "PowerB
         plot_all_groups_dark(graphgroups, deltaTime, title="Power Bands", yMin=-0.1, yMax=1.1)
     
     st.session_state["Button Pressed last"] = "PowerBands"
+    write_elapsed_time_till_start("Plot PowerBands Data")
 
 if st.button("Biometrics") or st.session_state["Button Pressed last"] == "Biometrics":
     data = osc_server.get_biometrics_dataframes().get_latest_frames(TIMESTEPS_WINDOW)
+    write_elapsed_time_till_start("Get Biometrics slice")
     graphs, deltaTime = get_graphs_and_deltaTime_from_slice(data)
 
     groups = [[ 'HeartBeatsPerMinute', 'BreathsPerMinute']]
     graphgroups = split_by_identifierGroups(graphs, groups, exclude="Pos")
+    write_elapsed_time_till_start("Prep Biometrics Data")
     plot_all_groups_dark(graphgroups, deltaTime, title="Biometrics", yMin=0, yMax=120)
+    write_elapsed_time_till_start("Plot Biometrics Data")
 
     st.session_state["Button Pressed last"] = "Biometrics"
 
@@ -79,10 +93,9 @@ if "waitTimeMultiplier" not in st.session_state:
 
 timeElapsed = time.time() - start_time
 timeElapsedMilliseconds = timeElapsed * 1000
-st.write("Time elapsed in [s]: ", timeElapsed)
-
 waitTime_mul = st.session_state["waitTimeMultiplier"]
 milliseconds_refresh_multiplied = MILLISECONDS_REFRESH * waitTime_mul
+
 
 if timeElapsedMilliseconds < milliseconds_refresh_multiplied:
     sleepTime = (milliseconds_refresh_multiplied - timeElapsedMilliseconds) / 1000
@@ -91,6 +104,8 @@ if timeElapsedMilliseconds < milliseconds_refresh_multiplied:
 else:
     st.warning("Time elapsed is greater than refresh rate. Increased refresh rate by " + str( waitTime_mul))
     st.session_state["waitTimeMultiplier"] = waitTime_mul + 0.2  
+
+    write_elapsed_time_till_start("Time elapsed total")
         
 st.rerun()
 
