300315703e311c18e806f50bfb9381ef771360dd
_Vulpes <vulpesk1tzune@gmail.com>
2025-02-27 15:03:41 +0100

    Restructure osc_server to provide list of datapoints in a safe and per key

--- dearpygui_viewer/osc_server.py
+++ dearpygui_viewer/osc_server.py
@@ -1,71 +1,102 @@
-
+import threading
 
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
+from collections import deque
 
 from osc_dataframes import OSCFrameDeque, OSCFrameCollector
 
-misc_collector = OSCFrameCollector()
-neurofb_collector = OSCFrameCollector()
-pwrbands_collector_left = OSCFrameCollector()
-pwrbands_collector_right = OSCFrameCollector()
-pwrbands_collector_avg = OSCFrameCollector()
-biometrics_collector = OSCFrameCollector()
+OSC_KEY_BASIS = "/avatar/parameters/BFI"
+MAX_STORED_TIMESTEPS = 20
 
-def bfi_data_handler_generic(path, value):
-    misc_collector.process_osc_message(path, value)
+OSC_PATHS_TO_KEY = { 
+    "/avatar/parameters/BFI/NeuroFB/FocusLeft": "NeuroFB_FocusLeft",
+    "/avatar/parameters/BFI/NeuroFB/FocusRight": "NeuroFB_FocusRight",
+    "/avatar/parameters/BFI/NeuroFB/FocusAvg": "NeuroFB_FocusAvg",
+    "/avatar/parameters/BFI/NeuroFB/RelaxLeft": "NeuroFB_RelaxLeft",
+    "/avatar/parameters/BFI/NeuroFB/RelaxRight": "NeuroFB_RelaxRight",
+    "/avatar/parameters/BFI/NeuroFB/RelaxAvg": "NeuroFB_RelaxAvg",
+    "/avatar/parameters/BFI/PwrBands/Left/Delta": "PwrBands_Left_Delta",
+    "/avatar/parameters/BFI/PwrBands/Left/Theta": "PwrBands_Left_Theta",
+    "/avatar/parameters/BFI/PwrBands/Left/Alpha": "PwrBands_Left_Alpha",
+    "/avatar/parameters/BFI/PwrBands/Left/Beta": "PwrBands_Left_Beta",
+    "/avatar/parameters/BFI/PwrBands/Left/Gamma": "PwrBands_Left_Gamma",
+    "/avatar/parameters/BFI/PwrBands/Right/Delta": "PwrBands_Right_Delta",
+    "/avatar/parameters/BFI/PwrBands/Right/Theta": "PwrBands_Right_Theta",
+    "/avatar/parameters/BFI/PwrBands/Right/Alpha": "PwrBands_Right_Alpha",
+    "/avatar/parameters/BFI/PwrBands/Right/Beta": "PwrBands_Right_Beta",
+    "/avatar/parameters/BFI/PwrBands/Right/Gamma": "PwrBands_Right_Gamma",
+    "/avatar/parameters/BFI/PwrBands/Avg/Delta": "PwrBands_Avg_Delta",
+    "/avatar/parameters/BFI/PwrBands/Avg/Theta": "PwrBands_Avg_Theta",
+    "/avatar/parameters/BFI/PwrBands/Avg/Alpha": "PwrBands_Avg_Alpha",
+    "/avatar/parameters/BFI/PwrBands/Avg/Beta": "PwrBands_Avg_Beta",
+    "/avatar/parameters/BFI/PwrBands/Avg/Gamma": "PwrBands_Avg_Gamma",
+    "/avatar/parameters/BFI/Biometrics/HeartbeatsPerMinute": "Biometrics_HeartbeatsPerMinute",
+    "/avatar/parameters/BFI/Biometrics/BreathsPerMinute": "Biometrics_BreathsPerMinute",
+    "/avatar/parameters/BFI/Biometrics/OxygenPercent": "OxygenPercent"
+}
 
-def neurofb_data_handler(path, value):
-    neurofb_collector.process_osc_message(path, value)
 
-def pwrbands_data_handler_left(path, value):
-    pwrbands_collector_left.process_osc_message(path, value)
+OSC_LIMITS = {
+    "NeuroFB_FocusLeft": [-1.0, 1.0],
+    "NeuroFB_FocusRight": [-1.0, 1.0],
+    "NeuroFB_FocusAvg": [-1.0, 1.0],
+    "NeuroFB_RelaxLeft": [-1.0, 1.0],
+    "NeuroFB_RelaxRight": [-1.0, 1.0],
+    "NeuroFB_RelaxAvg": [-1.0, 1.0],
+    "PwrBands_Left_Delta": [0.0, 1.0],
+    "PwrBands_Left_Theta": [0.0, 1.0],
+    "PwrBands_Left_Alpha": [0.0, 1.0],
+    "PwrBands_Left_Beta": [0.0, 1.0],
+    "PwrBands_Left_Gamma": [0.0, 1.0],
+    "PwrBands_Right_Delta": [0.0, 1.0],
+    "PwrBands_Right_Theta": [0.0, 1.0],
+    "PwrBands_Right_Alpha": [0.0, 1.0],
+    "PwrBands_Right_Beta": [0.0, 1.0],
+    "PwrBands_Right_Gamma": [0.0, 1.0],
+    "PwrBands_Avg_Delta": [0.0, 1.0],
+    "PwrBands_Avg_Theta": [0.0, 1.0],
+    "PwrBands_Avg_Alpha": [0.0, 1.0],
+    "PwrBands_Avg_Beta": [0.0, 1.0],
+    "PwrBands_Avg_Gamma": [0.0, 1.0],
+    "Biometrics_HeartbeatsPerMinute": [0.0,255],
+    "Biometrics_BreathsPerMinute": [0.0, 255],
+    "OxygenPercent": [0.0, 100.0]   
+}
 
-def pwrbands_data_handler_right(path, value):
-    pwrbands_collector_right.process_osc_message(path, value)
+class ProtectedOSCBuffer:
+    def __init__(self, max_len):
+        self.lock = threading.Lock()
+        self.deque = deque(maxlen=max_len)
+        self.countingDataPoints = 0
 
-def pwrbands_data_handler_avg(path, value):
-    pwrbands_collector_avg.process_osc_message(path, value)
+osc_buffers = { key : ProtectedOSCBuffer(MAX_STORED_TIMESTEPS) for path, key in OSC_PATHS_TO_KEY.items() }
+osc_elapsed_time_buffer = ProtectedOSCBuffer(MAX_STORED_TIMESTEPS)
 
-def biometrics_data_handler(path, value):
-    biometrics_collector.process_osc_message(path, value)
+def write_to_osc_buffer(path, value):
+    osc_buffers[path].lock.acquire()
+    osc_buffers[path].deque.append(value)
+    osc_buffers[path].countingDataPoints += 1
+    osc_buffers[path].lock.release()
 
-def bfi_time_handler(path, value):
-    misc_collector.process_osc_deltatime(value)
-    neurofb_collector.process_osc_deltatime(value)
-    pwrbands_collector_left.process_osc_deltatime(value)
-    pwrbands_collector_right.process_osc_deltatime(value)
-    pwrbands_collector_avg.process_osc_deltatime(value)
-    biometrics_collector.process_osc_deltatime(value)
+def read_from_osc_buffer(path):
+    osc_buffers[path].lock.acquire()
+    data = list(osc_buffers[path].deque)
+    osc_buffers[path].lock.release()
+    return data
 
-def get_misc_dataframes() -> OSCFrameDeque:
-    return misc_collector.get_osc_dataframes()
+def _osc_elapsed_time_handler(path, value):
+    write_to_osc_buffer("SecondsSinceLastUpdate", value)
 
-def get_neurofb_dataframes() -> OSCFrameDeque:
-    return neurofb_collector.get_osc_dataframes()
-
-def get_pwrbands_dataframes_left() -> OSCFrameDeque:
-    return pwrbands_collector_left.get_osc_dataframes()
-
-def get_pwrbands_dataframes_right() -> OSCFrameDeque:
-    return pwrbands_collector_right.get_osc_dataframes()
-
-def get_pwrbands_dataframes_avg() -> OSCFrameDeque:
-    return pwrbands_collector_avg.get_osc_dataframes()
-
-def get_biometrics_dataframes() -> OSCFrameDeque:
-    return biometrics_collector.get_osc_dataframes()
+def _osc_message_data_handler(path, value):
+    key = OSC_PATHS_TO_KEY[path]
+    write_to_osc_buffer(key, value)
 
 def run_server(ip, port):
     dispatcher = Dispatcher()
     #dispatcher.map("/avatar/parameters/BFI/*", bfi_data_handler)
-    dispatcher.map("/avatar/parameters/BFI/Info/SecondsSinceLastUpdate", bfi_time_handler)
-    dispatcher.map("/avatar/parameters/BFI/NeuroFB/*", neurofb_data_handler)
-    dispatcher.map("/avatar/parameters/BFI/PwrBands/Left/*", pwrbands_data_handler_left)
-    dispatcher.map("/avatar/parameters/BFI/PwrBands/Right/*", pwrbands_data_handler_right)
-    dispatcher.map("/avatar/parameters/BFI/PwrBands/Avg/*", pwrbands_data_handler_avg)
-    dispatcher.map("/avatar/parameters/BFI/Biometrics/*", biometrics_data_handler)
-    
+    dispatcher.map("/avatar/parameters/BFI/Info/SecondsSinceLastUpdate", _osc_elapsed_time_handler)
+    dispatcher.map("/avatar/parameters/BFI/*", _osc_message_data_handler)
 
     server = osc_server.BlockingOSCUDPServer(
         (ip, port), dispatcher)
