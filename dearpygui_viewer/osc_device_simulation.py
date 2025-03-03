f153758a5e6d02216ba2320376f03e7820d62fda
_Vulpes <vulpesk1tzune@gmail.com>
2025-03-03 12:38:55 +0100

    remove debug prints from device simulation

--- dearpygui_viewer/osc_device_simulation.py
+++ dearpygui_viewer/osc_device_simulation.py
@@ -6,17 +6,13 @@ from math import sin
 
 SLEEP_TIME = 0.1 #seconds
 
-osc_keys = [ key for key in OSC_PATHS_TO_KEY.values()]
 osc_paths = [ key for key in OSC_PATHS_TO_KEY.keys()]
 
-N = len(osc_keys)
-
-def run_client(osc_keys):
+def run_client():
     client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
 
-    print(osc_paths[0])
+    print(osc_paths)
     assert isinstance(osc_paths[0], str)
-    assert(N > 0)
 
     def _get_debuggable_times(time = 0.0):
         return 0.33 if (time % 1.0) > 0.5 else 0.66 
@@ -25,9 +21,7 @@ def run_client(osc_keys):
         while True:
             for idx, path in enumerate(osc_paths):
                 client.send_message(path, _get_debuggable_times(time.time()) + 0.05 * idx)
-                print(f"Sent {path} with value { _get_debuggable_times(time.time()) + 0.05 * idx}")
 
-            print (f"Sleeping for {SLEEP_TIME} seconds")
             time.sleep(SLEEP_TIME)
     except KeyboardInterrupt:
         print("Shutting down testing client...")
@@ -35,5 +29,5 @@ def run_client(osc_keys):
         print("Done")
 
 if __name__ == "__main__":
-    run_client(osc_keys)  
+    run_client()  
         
