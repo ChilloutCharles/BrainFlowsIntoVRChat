4bf64d355978bb4d832f7d9912d7f54165d0109c
_Vulpes <vulpesk1tzune@gmail.com>
2025-03-07 13:38:12 +0100

    Add --port parameter to device_simulation

--- dearpygui_viewer/osc_device_simulation.py
+++ dearpygui_viewer/osc_device_simulation.py
@@ -1,5 +1,6 @@
 from pythonosc import udp_client
 from osc_server import OSC_PATHS_TO_KEY, ELAPSED_TIME_PATH
+import argparse
 
 import time
 
@@ -9,19 +10,19 @@ startTime = time.time()
 
 osc_paths = [ key for key in OSC_PATHS_TO_KEY.keys()]
 
-def run_client():
-    client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
+def run_client(port):
+    client = udp_client.SimpleUDPClient("127.0.0.1", port)
 
     print(osc_paths)
     assert isinstance(osc_paths[0], str)
 
     def _get_debuggable_times(time = 0.0):
-        return 0.33 if (time % 1.0) > 0.5 else 0.66 
+        return 0.75 if (time % 10.0) > 7.5 else 0.5 if (time % 10.0) > 5.0 else 0.25 if (time % 10.0) > 2.5 else 0.0
 
     try:
         while True:
             for idx, path in enumerate(osc_paths):
-                client.send_message(path, _get_debuggable_times(time.time() + 0.1 * idx))
+                client.send_message(path, _get_debuggable_times(time.time()))
 
             elapsedTime = time.time() - startTime
             client.send_message(ELAPSED_TIME_PATH, elapsedTime)
@@ -33,5 +34,11 @@ def run_client():
         print("Done")
 
 if __name__ == "__main__":
-    run_client()  
+
+    ## Parse argument port
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=9000, help="The port to listen on")
+    args = parser.parse_args()
+
+    run_client(port=args.port)  
         
