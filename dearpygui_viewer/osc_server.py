f78af39c42d7b06b86cb1c273331d7518bcb5d02
_Vulpes <vulpesk1tzune@gmail.com>
2025-03-20 15:21:53 +0100

    refactor server: removed protected buffer definition, restructured bmi definitions into subgraph structured data

--- dearpygui_viewer/osc_server.py
+++ dearpygui_viewer/osc_server.py
@@ -1,5 +1,3 @@
-import threading
-
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server, udp_client
 from ml_actions_buffer import MLActionsBuffer
@@ -8,12 +6,10 @@ from collections import deque
 import queue
 import time
 
-OSC_KEY_BASIS = "/avatar/parameters/BFI"
+OSC_BMI_KEY_BASIS = "/avatar/parameters/BFI"
 MAX_STORED_TIMESTEPS = 200
 
-ELAPSED_TIME_PATH = "/avatar/parameters/BFI/Info/SecondsSinceLastUpdate"
-
-OSC_PATHS_TO_KEY = { 
+BMI_PATHS_TO_KEY = { 
     "/avatar/parameters/BFI/NeuroFB/FocusLeft": "NeuroFB_FocusLeft",
     "/avatar/parameters/BFI/NeuroFB/FocusRight": "NeuroFB_FocusRight",
     "/avatar/parameters/BFI/NeuroFB/FocusAvg": "NeuroFB_FocusAvg",
@@ -41,84 +37,42 @@ OSC_PATHS_TO_KEY = {
     "/avatar/parameters/BFI/Biometrics/OxygenPercent": "OxygenPercent"
 }
 
-
-class MLActionBuffer:
-
-    def __init__(self, num_actions):
-        self.action_buffers = {}
-        self._key_actions_dict = {}
-        self.num_actions = 0
-        if num_actions <= 0:
-            raise ValueError("num_actions must be a non-negative integer.")
-        if num_actions > 16: 
-            num_actions = 16
-            print("viewer does not support more than 16 actions, reducing to 16...")
-        self.num_actions = num_actions
-        self._make_buffers(num_actions - 1) 
-        
-    def _make_buffers(self, num_actions):
-
-        pattern = "/avatar/parameters/BFI/Action"
-        self._key_actions_dict = { pattern + str(i) : "Action" + str(i) for i in range(num_actions + 1)}
-
-        for key in self._key_actions_dict.values():
-            self.action_buffers[key] = ProtectedOSCBuffer(MAX_STORED_TIMESTEPS)
-            self.action_buffers[key].deque.append((0.0, 0.0))
-
-    def read_from_osc_ml_action_buffer(self, path):
-        self.action_buffers[path].lock.acquire()
-        data = list(self.action_buffers[path].deque)[-1]
-        self.action_buffers[path].lock.release()
-        return data
-
-    def write_to_osc_ml_action_buffer(self, path, value):
-        self.action_buffers[path].lock.acquire()
-        self.action_buffers[path].deque.append((value, time.time()))
-        self.action_buffers[path].lock.release()
-
-
-OSC_LIMITS = {
-    "NeuroFB_FocusLeft": (-1.0, 1.0),
-    "NeuroFB_FocusRight": (-1.0, 1.0),
-    "NeuroFB_FocusAvg": (-1.0, 1.0),
-    "NeuroFB_RelaxLeft": (-1.0, 1.0),
-    "NeuroFB_RelaxRight": (-1.0, 1.0),
-    "NeuroFB_RelaxAvg": (-1.0, 1.0),
-    "PwrBands_Left_Delta": (0.0, 1.0),
-    "PwrBands_Left_Theta": (0.0, 1.0),
-    "PwrBands_Left_Alpha": (0.0, 1.0),
-    "PwrBands_Left_Beta": (0.0, 1.0),
-    "PwrBands_Left_Gamma": (0.0, 1.0),
-    "PwrBands_Right_Delta": (0.0, 1.0),
-    "PwrBands_Right_Theta": (0.0, 1.0),
-    "PwrBands_Right_Alpha": (0.0, 1.0),
-    "PwrBands_Right_Beta": (0.0, 1.0),
-    "PwrBands_Right_Gamma": (0.0, 1.0),
-    "PwrBands_Avg_Delta": (0.0, 1.0),
-    "PwrBands_Avg_Theta": (0.0, 1.0),
-    "PwrBands_Avg_Alpha": (0.0, 1.0),
-    "PwrBands_Avg_Beta": (0.0, 1.0),
-    "PwrBands_Avg_Gamma": (0.0, 1.0),
-    "Biometrics_HeartBeatsPerMinute": (0.0, 255.0),
-    "Biometrics_BreathsPerMinute": (0.0, 255.0),
-    "OxygenPercent": (0.0, 100.0)   
+BMI_KEY_TO_GRAPH_ID = {
+    "NeuroFB_FocusLeft": "NeuroFB",
+    "NeuroFB_FocusRight": "NeuroFB",
+    "NeuroFB_FocusAvg": "NeuroFB",
+    "NeuroFB_RelaxLeft": "NeuroFB",
+    "NeuroFB_RelaxRight": "NeuroFB",
+    "NeuroFB_RelaxAvg":  "NeuroFB",
+    "PwrBands_Left_Delta": "PwrBands",
+    "PwrBands_Left_Theta": "PwrBands",
+    "PwrBands_Left_Alpha": "PwrBands",
+    "PwrBands_Left_Beta": "PwrBands",
+    "PwrBands_Left_Gamma":  "PwrBands",
+    "PwrBands_Right_Delta": "PwrBands",
+    "PwrBands_Right_Theta": "PwrBands",
+    "PwrBands_Right_Alpha": "PwrBands",
+    "PwrBands_Right_Beta": "PwrBands",
+    "PwrBands_Right_Gamma": "PwrBands",
+    "PwrBands_Avg_Delta": "PwrBands",
+    "PwrBands_Avg_Theta": "PwrBands",
+    "PwrBands_Avg_Alpha": "PwrBands",
+    "PwrBands_Avg_Beta": "PwrBands",
+    "PwrBands_Avg_Gamma": "PwrBands",
+    "Biometrics_HeartBeatsPerMinute": "Biometrics",
+    "Biometrics_BreathsPerMinute": "Biometrics",
+    "OxygenPercent": "BiometricsPercent"
 }
 
-OSC_ACTION_LIMIT = (0.0, 1.0)
+GRAPH_ID_TO_LIMITS = {
+    "NeuroFB": (-1.0, 1.0),
+    "PwrBands": (0.0, 1.0),
+    "Biometrics": (0.0, 255.0),
+    "BiometricsPercent": (0.0, 100.0)   
+}
 
-class ProtectedOSCBuffer:
-    def __init__(self, max_len = 50):
-        self.lock = threading.Lock()
-        self.deque = deque(maxlen=max_len)
-
-osc_buffers = { key : ProtectedOSCBuffer(MAX_STORED_TIMESTEPS) for path, key in OSC_PATHS_TO_KEY.items() }
+osc_buffers = { key : ProtectedOSCBuffer(MAX_STORED_TIMESTEPS) for path, key in BMI_PATHS_TO_KEY.items() }
 forward_queue = queue.Queue()
-
-for osc_buffer in osc_buffers.values():
-    osc_buffer.deque.append((0.0,0.0)) # vuffer shaped (value, time)
-
-osc_elapsed_time_buffer = ProtectedOSCBuffer(MAX_STORED_TIMESTEPS)
-osc_elapsed_time_buffer.deque.append(0.0)
 
 def write_to_osc_buffer(path, value):
     osc_buffers[path].lock.acquire()
