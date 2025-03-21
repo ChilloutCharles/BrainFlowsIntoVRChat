022b320b7a87670d623f575899d91be80f9dce19
_Vulpes <vulpesk1tzune@gmail.com>
2025-03-21 18:04:09 +0100

    change parameter --num-ml-actions to actions like in model/intent, fix issues in actions integration. Now the actions graph is shown as last graph. It works now with the device simulation

--- dearpygui_viewer/dearpygui_viewer.py
+++ dearpygui_viewer/dearpygui_viewer.py
@@ -22,8 +22,8 @@ t_count_plots = 0
 def main(args):
 
     ml_action_buffer = None
-    if args.num_ml_actions > 0:
-        ml_action_buffer = MLActionsBuffer(args.num_ml_actions, osc_server.MAX_STORED_TIMESTEPS)
+    if args.actions > 0:
+        ml_action_buffer = MLActionsBuffer(args.actions, osc_server.MAX_STORED_TIMESTEPS)
 
     def run_osc_server(osc_ip, osc_port_listen, osc_port_forward, ml_action_buffer):
         osc_server.run_buffer_server(osc_ip, osc_port_listen, osc_port_forward, ml_action_buffer)  
@@ -67,11 +67,17 @@ def main(args):
                 data, time = osc_data_per_key[idx_label]
                 next_data[label] = (data, time)
 
+
+        return next_data
+
+    def fetch_action_data_and_timestep_for_keys(ml_action_buffer=None):
+
+        next_data = {}
+
         if ml_action_buffer is not None:
-            for key in ml_action_buffer._key_actions_dict.keys():
+            for key in ml_action_buffer._action_paths_to_key.values():
                 data, time = ml_action_buffer.read_from_osc_ml_action_buffer(key)
                 next_data[key] = (data, time)
-
 
         return next_data
 
@@ -94,7 +100,7 @@ def main(args):
     def make_dict_action_subgraph(ml_action_buffer):
         if ml_action_buffer is None:
             return {}
-        return { "Actions" : ( ml_action_buffer.get_action_limits(), list(ml_action_buffer._key_actions_dict.values())) }
+        return { "Actions" : ( ml_action_buffer.get_action_limits(), list(ml_action_buffer._action_paths_to_key.values())) }
 
 
     dpg.create_context()
@@ -108,7 +114,7 @@ def main(args):
             
             time.sleep(1)
             osc_labels = get_labels_from_osc()
-            action_labels = list(ml_action_buffer._key_actions_dict.values() if ml_action_buffer is not None else [])
+            action_labels = list(ml_action_buffer._action_paths_to_key.values() if ml_action_buffer is not None else [])
 
             plot_show = { osc_label : True for osc_label in osc_labels + action_labels}
             data_digital = { osc_label : deque(maxlen=DEQUEUE_SIZE) for osc_label in osc_labels + action_labels}
@@ -158,47 +164,39 @@ def main(args):
                     global t_count_plots
                     t_count_plots += 1
                     t_digital_plot = time.time() - time_viewer_started
-                    osc_new_data_dict = fetch_set_label_data_and_timestep_for_keys(osc_labels) 
 
-                    if len(osc_new_data_dict) <= 0:
-                            return
-
-                    def _update_subplots(index, key_subset: list[str]):
+                    def _update_subplots(index, key_subset: list[str], new_data_dict):
                         tag_x_axis = f"_bmi_plot_x_time_{index}"
-                        
+
                         dpg.set_axis_limits(tag_x_axis, t_digital_plot - 5, t_digital_plot)
 
-                        sub_new_labels = [ label for label in key_subset if label in osc_new_data_dict.keys()]
-
-                        if len(sub_new_labels) > 0:
-                            for idx, sub_label in enumerate(sub_new_labels):
+                        if len(key_subset) > 0:
+                            for idx, sub_label in enumerate(key_subset):
                                 assert sub_label in data_digital.keys()
                                 if plot_show[sub_label] :
                                     #time relative to t_digital_plor
-                                    x_relate = osc_new_data_dict[sub_label][1] - time_viewer_started
-                                    y = osc_new_data_dict[sub_label][0]
+                                    x_relate = new_data_dict[sub_label][1] - time_viewer_started
+                                    y = new_data_dict[sub_label][0]
                                     data_digital[sub_label].append([x_relate,y])
                                     dpg.set_value(sub_label,  [*zip(*data_digital[sub_label])])
                                     
-
                     for plot_subset_idx, (key, value) in enumerate(make_dict_subgraph_to_limit_and_label.items()):
                         (limit, key_subset) = value
-                        _update_subplots(plot_subset_idx, key_subset)
+                        new_data_dict = fetch_bmi_data_and_timestep_for_keys(key_subset)
+                        _update_subplots(plot_subset_idx, key_subset, new_data_dict)
 
                     for plot_subset_idx, (key, value) in enumerate(make_dict_action_subgraph_to_limit_and_label.items()):
                         (limit, key_subset) = value
-                        _update_subplots(plot_subset_idx, key_subset)
+                        new_data_dict = fetch_action_data_and_timestep_for_keys(ml_action_buffer)
+                        _update_subplots(plot_subset_idx + len(make_dict_subgraph_to_limit_and_label.keys()), key_subset, new_data_dict)
 
-                plotIndex = 0
                 for plot_subset_idx, (key, value) in enumerate(make_dict_subgraph_to_limit_and_label.items()):
                     (limit, key_subset) = value
                     setup_plot_with_limits_and_message_subset(plot_subset_idx, limit, key_subset)
-                    plotIndex += 1
 
                 for plot_subset_idx, (key, value) in enumerate(make_dict_action_subgraph_to_limit_and_label.items()):
                     (limit, key_subset) = value
-                    setup_plot_with_limits_and_message_subset(plotIndex, limit, key_subset)
-                    plotIndex += 1
+                    setup_plot_with_limits_and_message_subset(plot_subset_idx + len(make_dict_subgraph_to_limit_and_label.keys()), limit, key_subset)
 
                 with dpg.item_handler_registry(tag="handler_tag_ref"):
                     dpg.add_item_visible_handler(callback=_update_plot)
@@ -213,6 +211,6 @@ if __name__ == "__main__":
     parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP address for OSC messages")
     parser.add_argument("--port_listen", type=int, default=9010, help="The port to listen on")
     parser.add_argument("--port_forward", type=int, default=9000, help="The port to forward the data", required=False)
-    parser.add_argument("--num_ml_actions", type=int, default=0, help="The number of actions to be displayed", required=False)
+    parser.add_argument("--actions", type=int, default=0, help="Set number of actions to be viewed (max 16)", required=False)
     args = parser.parse_args()
     main(args)
